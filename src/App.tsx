08f6d182144ba8328b098eb73e5684bd894c82ad
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-29 20:58:02 -0300

    :construction: chore: app and index updated
    styles imported on app component and unnecessary stuff removed from index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// Routes
+import Routes from "./routes";
+// styles
+import "./styles/settings/colors.css";
+import "./styles/settings/gaps.css";
+import "./styles/generic/reset.css";
+import "./styles/base/elements.css";
+import "./styles/objects/container.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Routes />;
+};
 
 export default App;
