25c3a8b530ea1fe3c6bb0ad2763f8450169db589
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-29 20:58:45 -0300

    :lipstick: feat: Index page component created

---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+// components
+import Input from "./../../components/Input";
+// imgs
+import arrowIcon from "../../assets/images/icon-arrow.svg";
+
+const Index = () => {
+  return (
+    <main className="ip-tracker">
+      <div className="ip-tracker-container container">
+        <h1 className="ip-tracker-title">Ip Address Tracker</h1>
+
+        <form className="ip-tracker-form">
+          <Input name="ip-adress" onChange={() => {}} />
+          <button className="ip-tracker-submit">
+            <img
+              src={arrowIcon}
+              alt="Seta, icone do botão"
+              className="ip-tracker-submit-arrow"
+            />
+          </button>
+        </form>
+
+        <section className="ip-tracker-dashboard">
+          <div className="ip-tracker-dashboard-info">
+            <span>Ip Address</span>
+            <h3>192.212.174.101</h3>
+          </div>
+
+          <div className="ip-tracker-dashboard-info">
+            <span>Location</span>
+            <h3>Brooklyn, NY 100001</h3>
+          </div>
+
+          <div className="ip-tracker-dashboard-info">
+            <span>Timezone</span>
+            <h3>UTC - 05:00</h3>
+          </div>
+
+          <div className="ip-tracker-dashboard-info">
+            <span>ISP</span>
+            <h3>SpaceX Starlink</h3>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Index;
