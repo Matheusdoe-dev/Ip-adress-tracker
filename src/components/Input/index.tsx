bc6f91570391e53d79c07ceb77f9f9ceda9b96aa
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-30 11:56:26 -0300

    :lipstick: chore: Input component styled

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,6 @@
 import React, { InputHTMLAttributes } from "react";
+// styles
+import "./styles.css";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -9,8 +11,19 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const Input: React.FC<InputProps> = ({ name, label, value, ...props }) => {
   return (
     <div className="input-block">
-      {label && <label htmlFor={name}>{label}</label>}
-      <input type="text" name={name} id={name} value={value} {...props} />
+      {label && (
+        <label className="input-label" htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <input
+        className="input"
+        type="text"
+        name={name}
+        id={name}
+        value={value}
+        {...props}
+      />
     </div>
   );
 };
