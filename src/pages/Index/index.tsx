a175d48384ef40a649f5d495f78b7a7ea685f8b6
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-10-05 14:34:02 -0300

    :lipstick: chore: some components added
    Map from React-Leaflet, Footer and Dashboard added.

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,68 +1,83 @@
 import React from "react";
 // components
 import Input from "./../../components/Input";
+import Footer from "./../../components/Footer";
+import Dashboard from "./../../components/Dashboard";
+import { TileLayer, Marker, Map } from "react-leaflet";
+import L from "leaflet";
 // imgs
 import arrowIcon from "../../assets/images/icon-arrow.svg";
+import markerImg from "../../assets/images/icon-location.svg";
 // styles
 import "./styles.css";
 // hooks
 import useIpTracker from "../../hooks/ip-tracker";
 
+// custom icon to put on Marker in map
+const markerIcon = new L.Icon({
+  iconUrl: markerImg,
+  iconRetinaUrl: markerImg,
+  iconSize: [33, 43],
+});
+
 const Index = () => {
   const {
     ip,
-    setIp,
     handleSubmitIp,
-    ipAdress,
-    isp,
+    position,
     location,
+    isp,
     timezone,
+    ipAdress,
+    setIp,
+    setIpErrorStatus,
   } = useIpTracker();
 
   return (
-    <main className="ip-tracker">
-      <div className="ip-tracker-container container">
-        <h1 className="ip-tracker-title">IP Address Tracker</h1>
+    <>
+      <main className="ip-tracker">
+        <div className="ip-tracker-container container">
+          <h1 className="ip-tracker-title">IP Address Tracker</h1>
 
-        <form className="ip-tracker-form" onSubmit={handleSubmitIp}>
-          <Input
-            name="ip-adress"
-            placeholder="Search for any IP address or domain"
-            value={ip}
-            onChange={(e) => setIp(e.target.value)}
-          />
-          <button className="ip-tracker-submit">
-            <img
-              src={arrowIcon}
-              alt="Seta, icone do botão"
-              className="ip-tracker-submit-arrow"
+          <form className="ip-tracker-form" onSubmit={handleSubmitIp}>
+            <Input
+              aria-label="IP Address"
+              name="ip-adress"
+              placeholder="Search for any IP address"
+              value={ip}
+              onChange={(e) => {
+                setIpErrorStatus(false);
+                setIp(e.target.value);
+              }}
             />
-          </button>
-        </form>
+            <button className="ip-tracker-submit">
+              <img
+                src={arrowIcon}
+                alt="Seta, icone do botão"
+                className="ip-tracker-submit-arrow"
+              />
+            </button>
+          </form>
+          <Dashboard
+            location={location}
+            timezone={timezone}
+            isp={isp}
+            ipAdress={ipAdress}
+          />
+        </div>
+      </main>
 
-        <section className="ip-tracker-dashboard">
-          <div className="ip-tracker-dashboard-info">
-            <span>Ip Address</span>
-            <h3>{ipAdress}</h3>
-          </div>
+      <Map className="map-container" center={position} zoom={14}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-          <div className="ip-tracker-dashboard-info">
-            <span>Location</span>
-            <h3>{location}</h3>
-          </div>
+        <Marker position={position} icon={markerIcon} />
+      </Map>
 
-          <div className="ip-tracker-dashboard-info">
-            <span>Timezone</span>
-            <h3>{timezone}</h3>
-          </div>
-
-          <div className="ip-tracker-dashboard-info">
-            <span>ISP</span>
-            <h3>{isp}</h3>
-          </div>
-        </section>
-      </div>
-    </main>
+      <Footer />
+    </>
   );
 };
 
